c4f378cd4809c51578e1ab1910538e88f3c37950
DESKTOP-IK4MUI3\nj <87179471@qq.com>
2018-04-09 19:05:45 +0800

    文章列表样式优化

--- src/components/page/Index.vue
+++ src/components/page/Index.vue
@@ -2,12 +2,16 @@
   <div class="wrap main-wrap">
     <div class="content main">
       <div class="list" v-for="item in contents" :key="item._id.toString()">
-        <h3><a href="">{{ item.title }}</a></h3>
-        <p v-if="item.user">
-          <el-tag>{{ item.tag.name }}</el-tag>
-          作者：{{ item.user.username }} 发布时间：{{ item.createTime }} 阅读：{{ item.views }}</p>
-        <p>{{ item.description }}</p>
-        <p>
+        <h3 class="title">{{ item.title }}</h3>
+        <p class="content-info clearfloat">
+          <span v-if="item.tag" class="tag-info"><el-tag size="medium">{{ item.tag.name }}</el-tag></span>
+          <span v-if="item.user" class="user-info"><i class="el-icon-edit"></i>{{ item.user.username }}</span>
+          <span v-else class="user-info"><i class="el-icon-edit"></i>佚名</span>
+          <span v-if="item.createTime" class="time-info"><i class="el-icon-time"></i>{{ item.createTime }}</span>
+          <span v-if="item.views >= 0" class="view-info"><i class="el-icon-view"></i>{{ item.views }}</span>
+        </p>
+        <p class="description">{{ item.description }}</p>
+        <p class="read">
           <router-link :to="{ path: '/content', query: { id: item._id.toString() }}">
             <el-button size="medium">阅读全文</el-button>
           </router-link>
@@ -55,6 +59,65 @@ export default {
 
 <style scoped>
   .main-wrap{
-    background-color: #eee;
+    background-color: #f6f6f6;
+    padding: 20px 0;
+  }
+  .list{
+    padding: 20px;
+    background-color: #fff;
+    margin-bottom: 16px;
+    border-radius: 5px;
+    box-shadow: 0px 2px 2px #ddd;
+  }
+/*  .list:after{
+    content: '';
+    display: block;
+    width: 100%;
+    height: 1px;
+    background-color: #ddd;
+    margin-top: 20px;
+  }*/
+  .title{
+    line-height: 36px;
+    text-align: center;
+  }
+  .content-info{
+    line-height: 36px;
+    color: #8590a6;
+  }
+  .content-info span{
+    display: block;
+    float: left;
+    margin-right: 20px;
+  }
+  .content-info i{
+    font-size: 18px;
+    margin-right: 6px;
+  }
+  .tag-info{
+    width: 100px;
+  }
+  .user-info{
+    width: 120px;
+  }
+  .user-info i{
+    color: #333;
+  }
+  .time-info{
+    width: 210px;
+  }
+  .time-info i{
+    color: #90e2fe;
+  }
+  .view-info{
+    width: 80px;
+  }
+  .view-info i{
+    color: #43d480;
+  }
+  .description{
+    color: #6d6d6d;
+    margin: 14px 0;
+    line-height: 22px;
   }
 </style>
