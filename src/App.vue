d8cd48e4d02346f4aa3035c3cc1dc4b1d39b1dd2
DESKTOP-IK4MUI3\nj <87179471@qq.com>
2018-05-18 12:24:30 +0800

    修复富文本pre code文本溢出

--- src/App.vue
+++ src/App.vue
@@ -113,4 +113,11 @@ a{
 .txt ol li{
   list-style-type: decimal;
 }
+.txt pre, .txt code{
+  white-space: pre-wrap; /* css-3 */
+  white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+  white-space: -pre-wrap; /* Opera 4-6 */
+  white-space: -o-pre-wrap; /* Opera 7 */
+  word-wrap: break-word; /* Internet Explorer 5.5+ */
+}
 </style>
